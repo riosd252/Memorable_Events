2499d190196487ce339b641cae8d11d7448dc379
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-02-23 14:47:34 +0100

    User class now implements UserDetails to comply with Spring Security.

--- src/main/java/DavidRios/Memorable_Events/entities/User.java
+++ src/main/java/DavidRios/Memorable_Events/entities/User.java
@@ -1,19 +1,22 @@
 package DavidRios.Memorable_Events.entities;
 
 import DavidRios.Memorable_Events.enums.Roles;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+@JsonIgnoreProperties({"password", "credentialsNonExpired", "accountNonExpired", "authorities", "accountNonLocked", "enabled"})
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Setter(AccessLevel.NONE)
@@ -31,14 +34,44 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "event_id"))
     private Set<Event> reservedEvents = new HashSet<>();
 
-    public User (String username, String password, String fullName, String email, Roles role) {
+    public User (String username, String password, String fullName, String email) {
 
         this.username = username;
         this.password = password;
         this.fullName = fullName;
         this.email = email;
-        this.role = role;
+        this.role = Roles.CUSTOMER;
 
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
