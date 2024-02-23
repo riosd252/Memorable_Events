0d837276ba0a2a45e7782070578be1aff64768b4
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-02-23 14:41:42 +0100

    Adds UserService and new constructor on NotFoundException to handle errors when users can't be found by id.

--- src/main/java/DavidRios/Memorable_Events/exceptions/NotFoundException.java
+++ src/main/java/DavidRios/Memorable_Events/exceptions/NotFoundException.java
@@ -1,7 +1,14 @@
 package DavidRios.Memorable_Events.exceptions;
 
+import java.util.UUID;
+
 public class NotFoundException extends RuntimeException {
     public NotFoundException (String message) {
         super(message);
+
+    }
+
+    public NotFoundException(UUID id) {
+        super("User identified as " + id + " was not found.");
     }
 }
---
+++ src/main/java/DavidRios/Memorable_Events/services/UserService.java
@@ -0,0 +1,47 @@
+package DavidRios.Memorable_Events.services;
+
+import DavidRios.Memorable_Events.entities.User;
+import DavidRios.Memorable_Events.exceptions.NotFoundException;
+import DavidRios.Memorable_Events.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public Page<User> getUsers(int pageNumber, int size, String orderBy) {
+        if (size > 100) size = 100;
+        Pageable pageable = PageRequest.of(pageNumber, size, Sort.by(orderBy));
+        return userRepository.findAll(pageable);
+    }
+
+
+    public User findById(UUID id) {
+        return userRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    public User update(UUID id, User modifiedUser) {
+        User found = this.findById(id);
+        found.setUsername(modifiedUser.getUsername());
+        found.setPassword(modifiedUser.getPassword());
+        found.setFullName(modifiedUser.getFullName());
+        found.setEmail(modifiedUser.getEmail());
+        return userRepository.save(found);
+    }
+
+    public void delete(UUID id) {
+        User found = this.findById(id);
+        userRepository.delete(found);
+    }
+
+}
