83ac030628ffb0cbdf2f426ca7b062126ad2b51c
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-02-23 11:40:32 +0100

    Initial commit

---
+++ src/main/java/DavidRios/Memorable_Events/MemorableEventsApplication.java
@@ -0,0 +1,13 @@
+package DavidRios.Memorable_Events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorableEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorableEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DavidRios/Memorable_Events/MemorableEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package DavidRios.Memorable_Events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorableEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
