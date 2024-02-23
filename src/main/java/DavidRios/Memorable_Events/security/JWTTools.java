d9e2b6bfaa4d1c4d3043ece224c7f06faee3beb6
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-02-23 14:24:13 +0100

    Adds JWTTools service.

---
+++ src/main/java/DavidRios/Memorable_Events/security/JWTTools.java
@@ -0,0 +1,41 @@
+package DavidRios.Memorable_Events.security;
+
+import DavidRios.Memorable_Events.entities.User;
+import DavidRios.Memorable_Events.exceptions.UnauthorizedException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JWTTools {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String createToken(User user) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .subject(String.valueOf(user.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception ex) {
+            throw new UnauthorizedException("Invalid token. Please try logging back in.");
+        }
+    }
+
+    public String extractIdFromToken(String token) {
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build().parseSignedClaims(token).getPayload()
+                .getSubject();
+    }
+
+}
